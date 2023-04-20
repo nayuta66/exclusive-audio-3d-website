55f822bc3ba416cda710340463c316be8412ca99
nayuta <nayuta66@163.com>
2023-04-20 17:08:39 +0800

    fix：添加loading动画

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,33 +1,73 @@
-import { useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './index.less'
-import * as three from 'three';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import WebGL from 'three/examples/jsm/capabilities/WebGL';
-import tween from '@tweenjs/tween.js';
+import TWEEN from '@tweenjs/tween.js';
 
 const HomePage = () => {
 
-    const scene = new three.Scene();
+    const [loading, setLoading] = useState(true);
+
+    const scene = new THREE.Scene();
     // fov视野角度, aspect长宽比, near近截面, far远截面
-    const camera = new three.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new three.WebGLRenderer({ antialias: true });
+    const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
     const gltfLoader = new GLTFLoader();
-    const textureLoader = new three.TextureLoader();
+    const textureLoader = new THREE.TextureLoader();
     const controls = new OrbitControls(camera, renderer.domElement);
-    const clock = new three.Clock();
+    const tween = new TWEEN.Tween(camera.position);
+    const clock = new THREE.Clock();
 
     // 3D模型
     const model = useRef<any>();
     // 动画混合器
     const mixer = useRef<any>();
+    // 光线投射鼠标拾取
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+
+    // 鼠标事件查找
+    const findEvent = (object: THREE.SkinnedMesh | THREE.Object3D) => {
+        let parent = object.parent;
+        let name = parent?.name;
+        while (parent) {
+            parent = parent.parent;
+            name = parent?.name;
+        }
+        return name;
+    };
+    // 鼠标事件执行
+    const executeEvent = (name: string | undefined) => {
+        console.log(name);
+    }
+
+    const onMouseClick = (event: MouseEvent) => {
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+        // 通过摄像机和鼠标位置更新射线
+        raycaster.setFromCamera(mouse, camera);
+        const intersects = raycaster.intersectObjects(scene.children);
+        if (intersects.length > 0) {
+            // 获取第一个交叉物体
+            const obj = intersects[0].object;
+            const name = findEvent(obj);
+            executeEvent(name);
+        }
+    }
 
     // 循环渲染
     const animate = () => {
-        controls.update();
         requestAnimationFrame(animate);
+        // 轨道更新
+        controls.update();
+        // 获取delta时间
         const delta = clock.getDelta();
+        // 更新动画
         mixer.current && mixer.current.update(delta);
+        // 补间动画更新
+        TWEEN.update();
         renderer.render(scene, camera);
     };
 
@@ -35,17 +75,18 @@ const HomePage = () => {
     useEffect(() => {
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.outputEncoding = three.sRGBEncoding;
+        renderer.outputEncoding = THREE.sRGBEncoding;
         textureLoader.load('texture/musician_cyber_bedroom.jpg', (texture) => {
-            texture.encoding = three.sRGBEncoding;
-            texture.mapping = three.EquirectangularReflectionMapping;
+            texture.encoding = THREE.sRGBEncoding;
+            texture.mapping = THREE.EquirectangularReflectionMapping;
             scene.background = texture;
         }, undefined, (error) => {
             console.error(error);
         });
 
         // 控制器配置初始化
-        controls.enableZoom = false;
+        controls.enableZoom = true;
+        controls.enablePan = true;
 
         // 将canvas添加到dom中
         const el = document.getElementById('homepage-canvas');
@@ -54,27 +95,36 @@ const HomePage = () => {
         // 加载模型
         gltfLoader.load('gltf/robot_playground/scene.gltf', (gltf) => {
             model.current = gltf.scene;
+            model.current.name = 'robot_playground';
             scene.add(model.current);
 
-            // 加载动画
+            // 加载模型动画
             const animations = gltf.animations;
-            mixer.current = new three.AnimationMixer(gltf.scene);
+            mixer.current = new THREE.AnimationMixer(gltf.scene);
             if (animations && animations.length > 0) {
-                animations.forEach((clip: three.AnimationClip) => {
+                animations.forEach((clip: THREE.AnimationClip) => {
                     mixer.current.clipAction(clip).play();
                 });
             }
+
+            // 停止加载loading
+            setLoading(false);
+
         }, undefined, (error) => {
             console.error(error);
         });
 
         // 设置相机初始位置
-        camera.position.set(0, 1, 3);
+        camera.position.set(0, 0, 3);
+
+        // 设置初始补间动画
+        tween.to({ x: 0, y: 2 }, 3000);
+        tween.start();
 
         // 加载灯光：环境光、平行光（产生阴影）
-        const light = new three.AmbientLight(0xffffff); // soft white light scene.add( light );
+        const light = new THREE.AmbientLight(0xffffff); // soft white light scene.add( light );
         scene.add(light);
-        const directionalLight = new three.DirectionalLight(0xffffff, 1);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
         scene.add(directionalLight);
 
         // webgl兼容性测试和animate执行
@@ -89,14 +139,22 @@ const HomePage = () => {
         window.addEventListener('resize', () => {
             renderer.setSize(window.innerWidth, window.innerHeight);
         });
-        return window.removeEventListener('resize', () => { });
+        // 监控鼠标点击事件
+        window.addEventListener('click', onMouseClick, false);
+        return () => {
+            window.removeEventListener('resize', () => { });
+            window.removeEventListener('click', () => { });
+        }
     }, []);
 
     return <div className="homepage">
-        <div id='homepage-canvas'></div>
-        <div id='homepage-content'>
-        </div>
-    </div>
+        <div id='homepage-canvas' style={{ visibility: loading ? 'hidden' : 'visible' }}></div>
+        {loading ?
+            <div className='homepage-loading'>
+                <img className='homepage-loading-img' src={require('@/asserts/images/loading.gif')} />
+            </div>
+            : <></>}
+    </div >
 }
 
 export default HomePage;
